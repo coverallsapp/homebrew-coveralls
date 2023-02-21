92fa30c665adb50a08276aba70ec1ae979821701
Valentin Kiselev <mrexox@evilmartians.com>
2023-02-21 12:41:22 +0300

    coveralls: 0.2.1

--- Formula/coveralls.rb
+++ Formula/coveralls.rb
@@ -1,20 +1,23 @@
 class Coveralls < Formula
   desc "Self-contained, universal coverage uploader binary for Coveralls"
   homepage "https://github.com/coverallsapp/coverage-reporter"
-  url "https://github.com/coverallsapp/coverage-reporter/archive/refs/tags/v0.1.8.tar.gz"
-  sha256 "a4499960c70dcb7746b45446b1ac4c15ff8cc77d576c0549969af884b27086f2"
+  url "https://github.com/coverallsapp/coverage-reporter/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "9b7263a310bf9df743d9668b10c0ae3a5ab7465c3927d4e7ee49d54243b4eb80"
   license "MIT"
 
-  depends_on "crystal"
+  depends_on "crystal" => :build
   depends_on "libyaml"
+  depends_on "libevent"
+  depends_on "pcre"
 
   def install
-    system "shards", "install"
-    system "crystal", "build", "src/cli.cr", "-o", "dist/coveralls", "--release", "--no-debug", "--progress"
-    bin.install "dist/coveralls"
+    system "shards", "install", "--production"
+    system "crystal", "build", "src/cli.cr", "-o", "./dist/coveralls", "--release", "--no-debug"
+    system "strip", "./dist/coveralls"
+    bin.install "./dist/coveralls"
   end
 
   test do
-    # assert_match version, shell_output("#{bin}/coveralls --version")
+    assert_match version, shell_output("#{bin}/coveralls --version")
   end
 end
