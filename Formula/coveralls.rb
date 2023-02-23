8f7ff65362f628dfc73824020517cfe7cf15956c
Valentin Kiselev <mrexox@evilmartians.com>
2023-02-23 11:58:08 +0300

    coveralls: 0.2.2

--- Formula/coveralls.rb
+++ Formula/coveralls.rb
@@ -1,8 +1,8 @@
 class Coveralls < Formula
   desc "Self-contained, universal coverage uploader binary for Coveralls"
   homepage "https://github.com/coverallsapp/coverage-reporter"
-  url "https://github.com/coverallsapp/coverage-reporter/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "9b7263a310bf9df743d9668b10c0ae3a5ab7465c3927d4e7ee49d54243b4eb80"
+  url "https://github.com/coverallsapp/coverage-reporter/archive/refs/tags/v0.2.2.tar.gz"
+  sha256 "144340394df686ce4fcce857dbfc74576aab970ea4ae59ce99bb5f6881c05046"
   license "MIT"
 
   depends_on "crystal" => :build
