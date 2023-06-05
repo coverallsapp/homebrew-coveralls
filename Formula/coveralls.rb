807e27a5e47261efe402642566270a622b49f8c4
Valentin Kiselev <20262425+mrexox@users.noreply.github.com>
2023-06-05 07:16:40 +0000

    coveralls: update 0.4.5 bottle.

--- Formula/coveralls.rb
+++ Formula/coveralls.rb
@@ -6,10 +6,10 @@ class Coveralls < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/coverallsapp/homebrew-coveralls/releases/download/coveralls-0.4.4"
-    sha256 cellar: :any,                 monterey:     "f461307251c8d03b765df79bc1959e3b73e74b550d6a1385e02b4680e5091cfc"
-    sha256 cellar: :any,                 big_sur:      "86d5be275b1a42fc53d58412b927241d674d22892a0ecb75ed51e19d322cc903"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "ec2b7527303ecfd06d680bc99979996fa5915f13ac1c12cbb44ce189dca4408e"
+    root_url "https://github.com/coverallsapp/homebrew-coveralls/releases/download/coveralls-0.4.5"
+    sha256 cellar: :any,                 monterey:     "c82a42294112cb010228bb2b622507ca9c2f8d7bec8c069d22f650327ad3d94a"
+    sha256 cellar: :any,                 big_sur:      "34a359853c240f7375343187cc8ffacbf80c539e8e33466e112ebf28095e4fc0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "96f7bd72df66af9f8324db6f5728c27b1917a70abae3c0216c7b2ff980127b74"
   end
 
   depends_on "crystal" => :build
