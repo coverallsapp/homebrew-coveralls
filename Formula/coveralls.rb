fa7994020bd1aab4036b467093f8bb8ce29fcccc
James Kessler <afinetooth@gmail.com>
2026-08-19 00:55:19 +0000

    coveralls 0.6.20

--- Formula/coveralls.rb
+++ Formula/coveralls.rb
@@ -1,8 +1,8 @@
 class Coveralls < Formula
   desc "Self-contained, universal coverage uploader for Coveralls"
   homepage "https://github.com/coverallsapp/coverage-reporter"
-  url "https://github.com/coverallsapp/coverage-reporter/archive/refs/tags/v0.6.19.tar.gz"
-  sha256 "6d61c07ed563229660d81b3c9eb899253180bfc911553eb382c625af88f15549"
+  url "https://github.com/coverallsapp/coverage-reporter/archive/refs/tags/v0.6.20.tar.gz"
+  sha256 "3c294b1ce0af462b844680416fc3e352bc3b3fb8e56e0d1e3ff3f0d948c2699e"
   license "MIT"
 
   bottle do
